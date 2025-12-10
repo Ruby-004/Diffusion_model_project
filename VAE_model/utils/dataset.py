fbb57d33063ed7b542cc385b3ebca7569921e430
Ruby-004 <dewerkerruben@gmail.com>
2025-12-10 11:47:45 +0100

    Update dataset.py

--- VAE_model/utils/dataset.py
+++ VAE_model/utils/dataset.py
@@ -116,19 +116,19 @@ class MicroFlowDataset(Dataset):
         # Pad depth to 12 (divisible by 4 for two stride-2 downsamples)
         microstructure = self.data['microstructure'][idx].permute(1, 0, 2, 3).float()  # [C, D, H, W]
         
-        # Input: 2D flow field (only x,y components, z is null)
-        velocity_input = self.data['velocity_input'][idx, :, [0,1], :, :].permute(1, 0, 2, 3).float()  # [2, D, H, W]
+        # Input: 2D flow field (all 3 components, z is null)
+        velocity_input = self.data['velocity_input'][idx].permute(1, 0, 2, 3).float()  # [3, D, H, W]
         
-        # Target: 3D flow field (only x,y components)
-        velocity_target = self.data['velocity'][idx, :, [0,1], :, :].permute(1, 0, 2, 3).float()  # [2, D, H, W]
+        # Target: 3D flow field (all 3 components)
+        velocity_target = self.data['velocity'][idx].permute(1, 0, 2, 3).float()  # [3, D, H, W]
         
         pressure = self.data['pressure'][idx].permute(1, 0, 2, 3).float()  # [C, D, H, W]
         
         # Pad depth dimension from 11 to 12 by replicating the last slice
         import torch.nn.functional as F
         microstructure = F.pad(microstructure, (0, 0, 0, 0, 0, 1), mode='replicate')  # [C, 12, H, W]
-        velocity_input = F.pad(velocity_input, (0, 0, 0, 0, 0, 1), mode='replicate')  # [2, 12, H, W]
-        velocity_target = F.pad(velocity_target, (0, 0, 0, 0, 0, 1), mode='replicate')  # [2, 12, H, W]
+        velocity_input = F.pad(velocity_input, (0, 0, 0, 0, 0, 1), mode='replicate')  # [3, 12, H, W]
+        velocity_target = F.pad(velocity_target, (0, 0, 0, 0, 0, 1), mode='replicate')  # [3, 12, H, W]
         pressure = F.pad(pressure, (0, 0, 0, 0, 0, 1), mode='replicate')  # [C, 12, H, W]
         
         sample = {
